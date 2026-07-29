667d374c085b6d59760d6ca9c2e29b2c7d4ad88d
monish6708 <136656105+monish6708@users.noreply.github.com>
2026-07-29 21:08:45 +0530

    Time: 12 ms (28.09%), Space: 50.2 MB (38.18%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+
+        int low= getmax(weights);
+        int high= getsum(weights);
+        while(low <high){
+            int mid = (low+high)/2;
+            int day= days(weights,mid);
+            if(day<= days){
+                high=mid;
+            }else{
+                low=mid+1;
+            }
+
+
+        }
+        return low;
+
+
+        
+    }
+
+    public int days(int[] weights,int mid){
+        int days=1;
+        int load=0;
+        for(int i=0;i<weights.length;i++){
+            if(weights[i]+load > mid){
+                days+=1;
+                load=weights[i];
+            }else{
+                load+=weights[i];
+            }
+        }
+        return days;
+    }
+
+    public int getmax(int[] weights){
+        int max=0;
+        for(int num: weights){
+            max= Math.max(max,num);
+        }
+        return max;
+    }
+
+    public int getsum(int[] weights){
+        int sum=0;
+        for(int num:weights){
+            sum+=num;
+        }
+        return sum;
+    }
+}
